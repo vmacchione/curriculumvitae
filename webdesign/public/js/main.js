7315d8f05139cb0373b9181d6c271c6c184971e6
vmacchione <vicente.macchione1@gmail.com>
2021-08-11 01:22:12 -0300

    Funcionalidad al BurgerMenu

---
+++ webdesign/public/js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener('load',() => {
+    let burgerMenu = document.querySelector('.navBar__linkBurger');
+    
+
+
+    /*
+    let link = document.querySelectorAll('.navBar__link')
+    let navBar = document.querySelector('.navBar')
+    let h
+
+    for (let i = 0; i < link.length ; i++ ){
+        link[i].addEventListener('click', function(){
+            navBar.style.display = 'none'
+        })
+    }*/
+
+})
