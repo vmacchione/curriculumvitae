9eee602914377d5388019a72c136ff54d2c0383c
vmacchione <vicente.macchione1@gmail.com>
2021-08-12 11:53:01 -0300

    Armado del burgerMenu

--- webdesign/public/js/main.js
+++ webdesign/public/js/main.js
@@ -1,17 +1,19 @@
-window.addEventListener('load',() => {
-    let burgerMenu = document.querySelector('.navBar__linkBurger');
-    
-
-
-    /*
-    let link = document.querySelectorAll('.navBar__link')
-    let navBar = document.querySelector('.navBar')
-    let h
-
-    for (let i = 0; i < link.length ; i++ ){
-        link[i].addEventListener('click', function(){
-            navBar.style.display = 'none'
+window.addEventListener('load',function(){
+    const btn_menu = document.querySelector('.btn_menu')
+    if(btn_menu){
+        btn_menu.addEventListener('click', () => {
+            const menu = document.querySelector('.navBar__ul')
+            menu.classList.toggle('show')
         })
-    }*/
-
+    }
+    
+    const link = document.querySelectorAll('.navBar__link')
+    for (let i = 0 ; i < link.length; i++){
+        link[i].addEventListener('click', function(){
+            console.log('Hola')
+            this.menu.classList.add('show')
+        })
+    }
+    
+    
 })
